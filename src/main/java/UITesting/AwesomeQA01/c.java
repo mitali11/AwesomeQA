77fd44d960ec4b9bed91e1db25b64b26d245bb95
Mitali <jmitali06@gmail.com>
2024-04-30 16:21:31 +0530

    added c.java

---
+++ src/main/java/UITesting/AwesomeQA01/c.java
@@ -0,0 +1,13 @@
+package UITesting.AwesomeQA01;
+
+/**
+ * Hello world!
+ *
+ */
+public class c 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
